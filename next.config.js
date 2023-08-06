8a99359ae8f64b904f850cede961a9952f8c72f4
Gfjelstad <82065221+Gfjelstad@users.noreply.github.com>
2023-08-06 11:20:57 -0500

    sd

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://fjeltec.com" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
